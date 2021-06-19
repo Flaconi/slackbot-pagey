5682a254730aa50ae0ce5429b37acf5f711f9e20
cytopia <cytopia@everythingcli.org>
2021-06-19 12:47:21 +0200

    Ensure to fetch PD when requested and not just once

--- pagey/__init__.py
+++ pagey/__init__.py
@@ -9,8 +9,10 @@ from .slack import *
 
 def main() -> None:
     """Run main entrypoint."""
+    # Parse command line arguments
     get_args()
 
+    # Ensure environment tokens are present
     try:
         SLACK_TOKEN = os.environ["PAGEY_SLACK_TOKEN"]
     except KeyError:
@@ -22,27 +24,25 @@ def main() -> None:
         print("Error, env variable 'PAGEY_PD_TOKEN' not set", file=sys.stdout)
         sys.exit(1)
 
-    # Initialize pagerduty
-    # Get schedules
-    schedules = fetch_schedules(PD_TOKEN)
-    response = ""
-    print(schedules)
-    for team, users in schedules.items():
-        response += f"*{team}*\n"
-        # Sort by escalation level
-        users.sort(key=lambda s: s["level"])
-        for user in users:
-            response += f"* [lvl: *{user['level']}* -> {user['until']}] {user['name']}\n"
-        response += "\n"
-
     # Slack command callback
+    # Add more commands into the if condition when required.
     def commandCallback(command: str) -> str:
         """This is a callback function for slack to evaluate response based on given command."""
+        # [Command: oncall] Get Pagerduty schedules
         if command.startswith("oncall"):
+            schedules = fetch_schedules(PD_TOKEN)
+            response = ""
+            for team, users in schedules.items():
+                response += f"*{team}*\n"
+                # Sort by escalation level
+                users.sort(key=lambda s: s["level"])
+                for user in users:
+                    response += f"* [lvl: *{user['level']}* -> {user['until']}] {user['name']}\n"
+                response += "\n"
             return response
         return "Available commands: oncall"
 
-    # Connect to Slack (RTM)
+    # Connect to Slack (RTM mode)
     slack = PageySlack(SLACK_TOKEN, commandCallback)
     if not slack.connect():
         print("Connection to Slack failed. Exception traceback printed above.", file=sys.stderr)
